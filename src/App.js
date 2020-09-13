6a5ecb874a87839c05609bb4bbe26f38493eec60
agomes96 <antoniofilipegomes96@gmail.com>
2020-09-13 10:49:58 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 import IcaoGenerator from "./components/IcaoGenerator"
 
 function App() {
+
+  useEffect(() => {
+    document.title = "ICAO Generator"
+ }, []);
+
   return (
     <IcaoGenerator />
   );
