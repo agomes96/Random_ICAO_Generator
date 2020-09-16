6571e676f877232ff18450ebc115083bef164626
agomes96 <antoniofilipegomes96@gmail.com>
2020-09-16 17:43:15 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random_icao_generator/precache-manifest.3f02446dca866ef628974360a2ad1e9c.js"
+  "/random_icao_generator/precache-manifest.0e9783a7e93ec0bc2cb442f2b1ee7b89.js"
 );
 
 self.addEventListener('message', (event) => {
